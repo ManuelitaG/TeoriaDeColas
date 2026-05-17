96a0d0725ce260166cac3a29a92d9ba81eea57d6
ManuelitaG <manuelagomezz1234chaparro@gmail.com>
2026-05-17 13:09:28 -0500

    Modelo MD1

---
+++ src/services/queue/md1.ts
@@ -0,0 +1,31 @@
+//Calculos del modelo M/D/1.
+import type { QueueInput, QueueResult } from "../../models/queue.types";
+
+export function calculateMD1 (data:QueueInput): QueueResult {
+    const { lambda, mu, s = 1} = data;
+
+    const rho = lambda / (s * mu);
+    const p0 = 1 - rho;
+
+    const Lq = (rho * rho) / (2 * p0);
+    const LqRounded = Math.ceil(Lq);
+
+    const L = LqRounded + (lambda/mu);
+    const LRounded = Math.ceil(L);
+
+    const Wq = LqRounded / lambda;
+    const W = Wq + (1/mu);
+
+    return{
+        rho,
+        p0,
+        Lq,
+        LqRounded,
+        L,
+        LRounded,
+        Wq,
+        W,
+        stable: rho < 1,
+    };
+
+}
